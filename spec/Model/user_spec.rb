a11162ed8cd95b14828512e3ac0935e4b5a9b175
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-04 17:55:07 -0500

    Added unit test for User

---
+++ spec/Model/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  let(:subject) do
+    described_class.new(
+      name: 'Santiago',
+    )
+  end
+
+  describe 'validations' do
+    it 'is valid with valid attribute' do
+      expect(subject).to be_valid
+    end
+
+    it 'The name of the user should exist' do
+      subject.name = ''
+      expect(subject).to_not be_valid
+    end
+
+    it 'The name of the user should be longer than 5 characters' do
+      subject.name = 'test'
+      expect(subject).to_not be_valid
+    end
+
+  end
+
+  describe 'Associations', type: :model do
+    it { should have_many(:savings) }
+    it { should have_many(:projects) }
+  end
+end
