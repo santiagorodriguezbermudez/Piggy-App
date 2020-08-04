6a65af98874f4072f1d3141cfc1a50e56051758b
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-04 17:34:08 -0500

    Added data to project migration and model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  has_many :savings, dependant: destroy
+  belongs_to :project_creator, class_name: 'User', foreign_key: :user_id
+
+  validates :name, :icon, presence: true
+  validates :name, length: { minimum: 5 }
+  validates :name, uniqueness: true
+  validates :goal, numericality: { greater_than_or_equal_to: 1 }
 end
--- db/migrate/20200804223053_create_projects.rb
+++ db/migrate/20200804223053_create_projects.rb
@@ -1,6 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
+      t.string :name
+      t.string :icon
+      t.decimal :goal
+
+      t.references :user, foreign_key:true
 
       t.timestamps
     end
