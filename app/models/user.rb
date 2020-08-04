a12b60aa7596609d7d1b5c891b26153ec5004fd4
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-04 17:36:28 -0500

    Added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :projects
+  has_many :savings, foreign_key: :author_id, class_name: 'Saving'
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 5 }
+  validates_uniqueness_of :name
 end
