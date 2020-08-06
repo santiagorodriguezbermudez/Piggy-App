d7e5f2c3f4fe9efc266b6f165a02925cf798cd05
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-06 15:34:07 -0500

    Added image to the Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,7 @@ class Project < ApplicationRecord
   validates :name, :icon, presence: true
   validates :name, length: { minimum: 5 }
   validates :goal, numericality: { greater_than_or_equal_to: 1 }
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_150752) do
+ActiveRecord::Schema.define(version: 2020_08_06_202954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2020_08_06_150752) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
