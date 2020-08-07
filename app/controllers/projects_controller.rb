ab8a95bb70736376360ba0d4bd32f6d4a00088de
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-07 12:31:54 -0500

    Implemented adding a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,9 +8,26 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user_id = current_user.id
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: "The Project #{@project.name} was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :image, :goal)
   end
 
 end
