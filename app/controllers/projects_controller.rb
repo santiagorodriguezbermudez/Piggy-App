a07bb3812395377a84f18cc1002ee4c0c2d37ceb
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-07 11:05:52 -0500

    Added controller for projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,17 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/new'
+  get 'projects/create'
+  get 'projects/destroy'
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
