06e248bcc7029abc1b890ef1d5942c0745c4b171
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-08 16:50:02 -0500

    Added all savings styling

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -1,7 +1,9 @@
 class SavingsController < ApplicationController
 
   def index
-    @savings = current_user.savings.savings_with_project.pluck(:name, :amount, :created_at).map {|s| {name: s[0], amount: s[1], date: s[2]}}
+    @savings = current_user.savings.savings_with_project.includes(:project)
+    @savings = @savings.pluck(:name, :amount, :created_at, 'projects.image_url', :project_id)
+    @savings.map! {|s| {name: s[0], amount: s[1], date: s[2].strftime("%b-%d-%Y"), img: s[3], project: s[4]}}
     @total_savings = current_user.savings.savings_with_project.sum(:amount)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :savings, foreign_key: :author_id, class_name: 'Saving'
 
   has_secure_password
-  has_attached_file :image, styles: { medium: '300x300#', thumb: '100x100#' }, default_url: 'test.svg'
+  has_attached_file :image, styles: { medium: '300x300#', thumb: '100x100#' }, default_url: 'user.svg'
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
   validates :name, presence: true
   validates :name, length: { minimum: 5 }
