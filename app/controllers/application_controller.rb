cd4ca0729e1cb51f1867b56c0a9b273d93f702dc
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-10 18:16:15 -0500

    Changed re routing for authorize method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    redirect_to login_url, notice: 'Please log in' unless User.find_by(id: session[:user_id])
+    redirect_to welcome_path unless User.find_by(id: session[:user_id])
   end
 end
