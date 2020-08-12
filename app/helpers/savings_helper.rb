23bc30ef69301ebb64843c5cf4dae998cd6a7cda
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-12 14:44:59 -0500

    Refactor form partial to include helper method

--- app/helpers/savings_helper.rb
+++ app/helpers/savings_helper.rb
@@ -7,6 +7,15 @@ module SavingsHelper
     collection_select(:saving, :project_id, Project.all, :id, :name, prompt: true).html_safe
   end
 
+  def show_projects
+    projects = Project.all.pluck(:name).flatten
+    if projects.empty?
+      (link_to 'Add the first Project', root_path, class: 'border-white w-100 rounded mb-0 py-4 pl-5 my-3').html_safe
+    else
+      collection_select(:saving, :project_id, Project.all, :id, :name, prompt: true).html_safe
+    end
+  end
+
   def message_if_no_projects(savings)
     if savings.empty? 
       "<div class='d-flex justify-content-between my-3 bg-white rounded p-5 shadow'>
