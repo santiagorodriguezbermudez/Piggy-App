9c9f5ed6eb1cfaf005e67b21b86a85efd75b4d94
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-12 14:24:21 -0500

    Refactor project view to include helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -10,4 +10,23 @@ module ProjectsHelper
   def goal_completed(project)
     (project_savings_amount_sum(Project.find(project[:id])) / Project.find(project[:id]).goal) * 100
   end
+
+  def message_if_no_projects(projects)
+  
+    if projects.empty? 
+      "<div class='d-flex justify-content-between my-3 bg-white rounded p-5 shadow'>\
+      <p>No projects registered</p>\
+      </div>".html_safe
+    end
+
+  end
+
+  def render_img(project)
+    
+    unless project[:img].nil?
+      
+      (link_to (image_tag project[:img], class:'profile text-white').html_safe, project_path(id: project[:id]), class:'p-5').html_safe
+      
+    end
+  end
 end
